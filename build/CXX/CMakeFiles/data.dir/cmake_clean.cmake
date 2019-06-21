4905123b3b3cc8fb356601f1130ec3e2d966e7ff
FDniel97 <f.dniel97@gmail.com>
2019-06-21 13:20:45 +0200

    removing build

--- build/CXX/CMakeFiles/data.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/data"
-  "../plot/data.dat"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/data.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
